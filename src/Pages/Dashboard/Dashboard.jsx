5d2656b0f6f2ecd8f4a9eb50359a34c39bcbc70e
Queen-Ora <>
2024-09-04 09:36:02 +0000

    first commit

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '../../Components/Button/Button'
+
+
+export default function Dashboard() {
+  return (
+    <div>
+          <h1>Dashboard</h1>
+          <Button text={ 'Se Déconnecter'} />
+    </div>
+  )
+}
